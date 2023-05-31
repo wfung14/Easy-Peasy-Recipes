8970e10a58ab9c3e245e5bcd7865f0aa9eee23ea
Wilson Fung <wilson_fung1996@yahoo.com>
2023-05-30 22:48:27 -0400

    made create and new functionality

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,9 +1,26 @@
 const Recipe = require('../models/recipe');
 
+const index = async (req,res) => {
+  res.render('recipes/index', {
+    recipes: await Recipe.find()
+  })
+}
+
 const newRecipe = (req, res) => {
   res.render('recipes/new', { errorMsg: ''});
 }
 
+const create = async (req, res) => {
+  try {
+    const recipe = await Recipe.create(req.body);
+    res.redirect('/recipes');
+  } catch (err) {
+    res.render('recipes/new', { errorMsg: err.message });
+  }
+}
+
 module.exports = {
   new: newRecipe,
+  create,
+  index
 };
--- routes/recipes.js
+++ routes/recipes.js
@@ -7,4 +7,10 @@ const recipesCtrl = require('../controllers/recipes')
 // GET /recipes/new
 router.get('/new', recipesCtrl.new);
 
+// POST /recipes
+router.post('/', recipesCtrl.create);
+
+// GET route for /recipes
+router.get('/', recipesCtrl.index);
+
 module.exports = router;
