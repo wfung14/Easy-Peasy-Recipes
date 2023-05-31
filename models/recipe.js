0295d37fc5764c490ddcca4fbd9b490bb0309927
Wilson Fung <wilson_fung1996@yahoo.com>
2023-05-31 10:09:15 -0400

    add commentSchema

--- models/recipe.js
+++ models/recipe.js
@@ -2,11 +2,20 @@ const mongoose = require('mongoose');
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema ({
+  content: {
+    type: String
+  }
+}, {
+  timestamps: true
+})
+
 const recipeSchema = new mongoose.Schema ({
   name: {type: String},
   description: {type: String},
   ingredients: {type: String},
-  instructions: {type: String}
+  instructions: {type: String},
+  comments: [commentSchema]
 })
 
 // Compile the schema into a model and export it
